eb20badd89791a93c60b932f452d3384c39ce653
blindy <ayoblindstone@gmail.com>
2021-04-12 08:20:08 +0300

    add bootstrap classes

--- resources/views/url/index.blade.php
+++ resources/views/url/index.blade.php
@@ -3,32 +3,38 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>URLS</h1>
-    <div>
-        @include('flash::message')
-    </div>
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    <td>ID</td>
-                    <td>Name</td>
-                    <td>Created at</td>
-                    <td>Updated at</td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($urls as $url)
+    <div class="container">
+        <a href="/">Home</a>
+        <h1>URLS</h1>
+        <div>
+            @include('flash::message')
+        </div>
+        <div>
+            <table class="table table-hover">
+                <thead>
                     <tr>
-                        <td>{{$url->id}}</td>
-                        <td><a href="{{route('urls.show', $url->id)}}">{{$url->name}}</td>
-                        <td>{{$url->created_at}}</td>
-                        <td>{{$url->updated_at}}</td>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Created at</th>
+                        <th scope="col">Updated at</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        {{$urls->links()}}
-    <div>
+                </thead>
+                <tbody>
+                    @foreach($urls as $url)
+                        <tr>
+                            <td>{{$url->id}}</td>
+                            <td><a href="{{route('urls.show', $url->id)}}">{{$url->name}}</td>
+                            <td>{{$url->created_at}}</td>
+                            <td>{{$url->updated_at}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <nav>
+                <ul class="pagination">
+                    {{$urls->links()}}
+                </ul>
+            </nav>
+        <div>
+    </div>
 </body>
