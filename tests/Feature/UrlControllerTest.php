64a5f780d59a5957b5891c50b9067a63a77d4724
bl1ndy <ayoblindstone@gmail.com>
2021-04-13 14:43:09 +0300

    add tests for routes

---
+++ tests/Feature/UrlControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class UrlControllerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Artisan::call('migrate');
+        Artisan::call('db:seed');
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get('/');
+
+        $response->assertOk();
+    }
+
+    public function testUrlsIndex()
+    {
+        $response = $this->get(route('urls.index'));
+        $response->assertOk();
+    }
+
+    public function testShow()
+    {
+        $response = $this->get(route('urls.show', ['url' => 1]));
+        $response->assertOk();
+    }
+
+    public function testStore()
+    {
+        $data = [
+            'url' => [
+                'name' => 'https://urlfortest.com/'
+            ]
+        ];
+
+        $response = $this->post(route('urls.store'), $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('urls', $data['url']);
+    }
+}
