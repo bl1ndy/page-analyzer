df47728eff072101246d6fe54b5a14dc99d34dbf
bl1ndy <ayoblindstone@gmail.com>
2021-04-14 18:00:09 +0300

    add checks list to url view

--- resources/views/url/show.blade.php
+++ resources/views/url/show.blade.php
@@ -2,10 +2,12 @@
 
 @section('content')
     <div class="container mt-4">
-        <h1>Site: {{$url[0]->name}}</h1>
-        <form action="/urls/{id}/checks" method="post">
+        <h1>Site: {{$url->name}}</h1>
+        <form action="/urls/{url}/checks" method="post">
             @csrf
-            <input type="button" value="Check URL">
+            <input type="hidden" name="id" value="{{$url->id}}">
+            <input type="submit" value="Check URL">
         </form>
+        @include('url.check.index')
     </div>
 @endsection
