c266729d4b7bf17236379c21c9704b10d04c2187
blindy <ayoblindstone@gmail.com>
2021-04-14 22:56:25 +0300

    add hidden input url_name into form

--- resources/views/url/show.blade.php
+++ resources/views/url/show.blade.php
@@ -5,7 +5,8 @@
         <h1>Site: {{$url->name}}</h1>
         <form action="/urls/{url}/checks" method="post">
             @csrf
-            <input type="hidden" name="id" value="{{$url->id}}">
+            <input type="hidden" name="url_name" value="{{$url->name}}">
+            <input type="hidden" name="url_id" value="{{$url->id}}">
             <input type="submit" value="Check URL">
         </form>
         @include('url.check.index')
