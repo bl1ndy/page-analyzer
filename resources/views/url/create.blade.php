4541c7b9fb5d511e3df05eb7c4432d7d3709bd8e
blindy <ayoblindstone@gmail.com>
2021-04-15 19:29:57 +0300

    change h1 content

--- resources/views/url/create.blade.php
+++ resources/views/url/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold">Page Analizator</h1>
+        <h1 class="display-5 fw-bold">Page Analyzer</h1>
         <div class="col-lg-6 mx-auto">
             <p class="lead mb-4">Check sites for SEO optimization</p>
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
