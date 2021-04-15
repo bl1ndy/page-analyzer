03cb21b282a41ac770d46b5ec9c0a2797509dc82
bl1ndy <ayoblindstone@gmail.com>
2021-04-15 11:58:25 +0300

    change page  title

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <title>Bl1ndy-Analizator</title>
+        <title>page analyzer by bl1ndy</title>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
