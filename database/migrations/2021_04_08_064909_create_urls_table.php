6aaa77c599b4fc95788f69155f3b1989a9e2c64e
blindy <ayoblindstone@gmail.com>
2021-04-15 22:03:50 +0300

    some fixes for final project check

--- database/migrations/2021_04_08_064909_create_urls_table.php
+++ database/migrations/2021_04_08_064909_create_urls_table.php
@@ -15,7 +15,7 @@ class CreateUrlsTable extends Migration
     {
         Schema::create('urls', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
             $table->timestamp('last_check')->nullable();
             $table->integer('last_status_code')->nullable();
