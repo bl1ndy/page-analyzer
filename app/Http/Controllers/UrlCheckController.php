80de1456d3cfb5fefbaf895d97119f1678190ed0
bl1ndy <ayoblindstone@gmail.com>
2021-04-15 11:59:22 +0300

    add getting h1, keywords and description from url

--- app/Http/Controllers/UrlCheckController.php
+++ app/Http/Controllers/UrlCheckController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use DiDom\Document;
+use DiDom\Query;
 use Carbon\Carbon;
 
 class UrlCheckController extends Controller
@@ -22,9 +24,25 @@ class UrlCheckController extends Controller
         $currentDate = Carbon::now()->toDateTimeString();
         $statusCode = Http::get($url)->status();
 
+        $document = new Document();
+        $document->loadHtmlFile($url);
+        $h1 = $document->has('h1')
+            ? $document->find('h1')[0]->text()
+            : null;
+        $keywords = $document->has('//meta[@name="keywords"]', 'XPATH')
+            ? $document->find('//meta[@name="keywords"]', Query::TYPE_XPATH)[0]->getAttribute('content')
+            : null;
+        $description = $document->has('//meta[@name="description"]', 'XPATH')
+            ? $document->find('//meta[@name="description"]', Query::TYPE_XPATH)[0]->getAttribute('content')
+            : null;
+
+
         DB::table('url_checks')->insert([
             'url_id' => $urlId,
             'status_code' => $statusCode,
+            'h1' => $h1,
+            'keywords' => $keywords,
+            'description' => $description,
             'created_at' => $currentDate,
             'updated_at' => $currentDate
         ]);
