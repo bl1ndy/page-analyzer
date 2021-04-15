e851a1fae5a99345c71eb0dacdc6c2fa548a0a78
bl1ndy <ayoblindstone@gmail.com>
2021-04-15 15:03:42 +0300

    remove errors handling

--- resources/views/url/create.blade.php
+++ resources/views/url/create.blade.php
@@ -4,16 +4,6 @@
     <div class="container mt-4">
         <h1>Page Analizator</h1>
         <div>
-            @if ($errors->any())
-                <div>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
             <form action="/" method="POST">
                 @csrf
                 <label for="name">Name</label>
