b5499e7dd0b18993ffbc4245f87568a9f5f9ba92
blindy <ayoblindstone@gmail.com>
2021-04-15 20:23:45 +0300

    some fixes for final project check

--- app/Http/Requests/StoreUrlRequest.php
+++ app/Http/Requests/StoreUrlRequest.php
@@ -22,7 +22,7 @@ class StoreUrlRequest extends FormRequest
     {
         return [
             'url.name.required' => 'URL is required',
-            //'url.name.unique' => 'URL is already exists',
+            'url.name.unique' => 'URL is already exists',
             'url.name.url' => 'Invalid url format'
         ];
     }
